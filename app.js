66a38a869c39c2130b8c812845236af2ee7f2967
Your Name <jordanmichelequinn@gmail.com>
2017-08-17 11:32:35 -0700

    minor edits to html and .js

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const bodyParser = require("body-parser");
 const shortid = require('shortid')
 //empty array to put new todos in
 let todos = []
-//seeded "completed todo to check if it shows up"
-let completed = ["dance"]
+
+let completed = []
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
@@ -20,31 +20,24 @@ app.use(bodyParser.urlencoded({extended:false}))
 //render the index page to add new todos and see completed todos
 app.get("/", function (req, res) {
   res.render('index', {todos: todos, completed: completed});
-  console.log(completed)
 });
 
 //input new todos
-app.post("/todo/add", function (req, res){
+app.post("/todo/add", function(req, res){
   todos.push({
     id: shortid.generate(),
-    newtodo: req.body.newtodo})
+    newtodo: req.body.newtodo
+  })
   res.redirect('/');
-      console.log(todos)
  })
-//put completed todos in new array but it doesn't work
- // app.post("/todo/complete", function (req, res){
- //   completed.push({
- //     completed: req.body.todos})
- //   res.redirect('/');
- //       console.log(completed)
- //  })
+
 
 app.post('/todo/complete', function(req, res, next){
   let id = req.body.id
   let completeTodo = todos.filter(function (item){
     return item.id == id
   })[0]
-  completed.push(completeTodo)
+  completed.push(completeTodo.newtodo)
 
   todos = todos.filter(function(item){
     return item.id !==id
